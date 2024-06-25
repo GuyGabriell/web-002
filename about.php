d5fc43e114892d1693c5081b8e0d7e56325ab71b
GuyGabriell <jonahgabriel85@yahoo.com>
2024-06-25 11:02:05 +0100

    php-tailwind-together

---
+++ about.php
@@ -0,0 +1,13 @@
+
+<?php 
+
+require 'functions.php';
+
+$heading = 'About Us';
+
+
+
+
+
+
+require "views/about.view.php";
---
+++ testing.php
@@ -0,0 +1,12 @@
+
+<?php 
+
+require 'functions.php';
+
+
+$heading = 'Testing';
+
+
+
+
+require "views/testing.view.php";
